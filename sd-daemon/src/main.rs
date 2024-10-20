218d3918da0f5b298cd3ca0365a0696916c67405
Einfach_Irgendwa1 <faris.karge@berlin>
2024-10-20 09:43:58 +0200

    project structure changed again, added some daemon code

--- sd-daemon/src/main.rs
+++ sd-daemon/src/main.rs
@@ -1,3 +1,16 @@
+use std::{net::TcpListener, thread};
+
+use anyhow::Context;
+use sd_lib::ADDRESS;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(ADDRESS)
+        .context(format!("Failed to bind to address {ADDRESS}."))
+        .unwrap();
+
+    for stream in listener.incoming() {
+        thread::spawn(move || {
+            let stream = stream.context("Connection failed!").unwrap();
+        });
+    }
 }
---
+++ sd-lib/src/lib.rs
@@ -0,0 +1,1 @@
+pub const ADDRESS: &'static str = "127.0.0.1:115100";
