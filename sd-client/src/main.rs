0341fcc805ed3929cc908e30cad1dedcbaabe468
Einfach_Irgendwa1 <faris.karge@berlin>
2024-10-23 09:12:47 +0200

    added some client code

--- sd-client/src/main.rs
+++ sd-client/src/main.rs
@@ -1,5 +1,5 @@
 use gtk::{
-    gio::ApplicationFlags, glib::ExitCode, prelude::*, Application, ApplicationWindow,
+    gio::ApplicationFlags, glib::ExitCode, prelude::*, Application, ApplicationWindow, Grid, Label,
     WindowPosition,
 };
 
@@ -7,14 +7,38 @@ fn main() -> ExitCode {
     let application = Application::new(None, ApplicationFlags::FLAGS_NONE);
 
     application.connect_activate(|app| {
+        let grid = Grid::new();
+
+        grid.set_column_homogeneous(false);
+
+        let level = &new_label("Level");
+        level.set_width_request(100);
+
+        let application = &new_label("Application");
+        application.set_width_request(1300);
+
+        let message = &new_label("Message");
+        message.set_width_request(520);
+
+        grid.attach(level, 0, 0, 1, 1);
+        grid.attach(application, 1, 0, 1, 1);
+        grid.attach(message, 2, 0, 1, 1);
+
+        grid.set_width_request(1920);
+
         ApplicationWindow::builder()
             .application(app)
             .title("Status Daemon")
             .window_position(WindowPosition::None)
             .decorated(false)
+            .child(&grid)
             .build()
             .show_all();
     });
 
     application.run()
+}
+
+fn new_label(content: &str) -> Label {
+    Label::new(Some(content))
 }
